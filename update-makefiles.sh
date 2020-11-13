df1dc3087fbb4ff666f632a9fef78a7bb153b829
Akash Srivastava <akashniki@gmail.com>
2020-11-13 12:21:02 +0100

    interface: Rename to lineage

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.p404:hardware/404/interfaces" \
+  "vendor.lineage:hardware/404/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
